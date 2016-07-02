c8b42a131a6ef543520393a43114f21f784a3d9e
Pierre Nagorny <pierre.nagorny@gmail.com>
2016-07-02 14:06:06 +0200

    Added wind & astronomy, conditionnal precipitation display, (Debug flag, transistion time speedup)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 /**The MIT License (MIT)
 
-Copyright (c) 2016 by Daniel Eichhorn
+Author a1rb4Ck https://github.com/a1rb4Ck
+Based on Daniel Eichhorn work https://github.com/squix78/ & http://blog.squix.ch
+
+CREDITS :
+OLEDDisplay SSD1306Wire ThingspeakClient TimeClient libraries, WeatherStation Fonts by Daniel Eichhorn
+Sun, moon and local time based on bHogan2 https://github.com/squix78/esp8266-weather-station/issues/32
+ESP8266_SSD1306 by Fabrice Weinberg & Daniel Eichhorn
+NTPClient by Fabrice Weinberg & Sandeep Mistry https://github.com/sandeepmistry
+WifiManager library by AlexT https://github.com/tzapu
+BME280 library by Tyler Glenn https://github.com/finitespace
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -20,8 +29,9 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
-See more at http://blog.squix.ch
 */
+
+#define DEBUG
 
 #include <ESP8266WiFi.h>
 #include <Ticker.h>
@@ -42,7 +52,6 @@ See more at http://blog.squix.ch
 #include "TimeClient.h"
 #include "ThingspeakClient.h"
 
-
 /***************************
  * Begin Settings
  **************************/
@@ -56,22 +65,22 @@ const int UPDATE_INTERVAL_SECS = 10 * 60; // Update every 10 minutes
 
 // Display Settings
 const int I2C_DISPLAY_ADDRESS = 0x3c;
-const int SDA_PIN = D2;
-const int SDC_PIN = D3;
+const int SDA_PIN = D3; // on Wemos D1 mini with an old SSD1306 Oled you can solder
+const int SDC_PIN = D4; // 5V-GND-SCL-SDA pins directly (or just switch 5V/GND)
 
 // TimeClient settings
 const float UTC_OFFSET = 2;
 
 // Wunderground Settings
 const boolean IS_METRIC = true;
-const String WUNDERGRROUND_API_KEY = "31b5cd03db3a0e0d";
-const String WUNDERGRROUND_LANGUAGE = "EN";
-const String WUNDERGROUND_COUNTRY = "CH";
-const String WUNDERGROUND_CITY = "Zurich";
+const String WUNDERGRROUND_API_KEY = "xxxxxxxxxxxxxxxx";
+const String WUNDERGRROUND_LANGUAGE = "FR";
+const String WUNDERGROUND_COUNTRY = "FR";
+const String WUNDERGROUND_CITY = "Annecy-le-Vieux";
 
 //Thingspeak Settings
-const String THINGSPEAK_CHANNEL_ID = "67284";
-const String THINGSPEAK_API_READ_KEY = "L2VIW20QVNZJBLAK";
+const String THINGSPEAK_CHANNEL_ID = "XXXXXX";
+const String THINGSPEAK_API_READ_KEY = "XXXXXXXXXXXXXXXX";
 
 // Initialize the oled display for address 0x3c
 // sda-pin=14 and sdc-pin=12
@@ -103,9 +112,12 @@ void drawOtaProgress(unsigned int, unsigned int);
 void updateData(OLEDDisplay *display);
 void drawDateTime(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y);
 void drawCurrentWeather(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y);
+void drawCurrentDetails(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y);
 void drawForecast(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y);
-void drawThingspeak(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y);
 void drawForecastDetails(OLEDDisplay *display, int x, int y, int dayIndex);
+void drawThingspeak(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y);
+void drawNight(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y);
+
 void drawHeaderOverlay(OLEDDisplay *display, OLEDDisplayUiState* state);
 void setReadyForWeatherUpdate();
 int8_t getWifiQuality();
@@ -114,18 +126,19 @@ int8_t getWifiQuality();
 // Add frames
 // this array keeps function pointers to all frames
 // frames are the single views that slide from right to left
-FrameCallback frames[] = { drawDateTime, drawCurrentWeather, drawForecast, drawThingspeak };
-int numberOfFrames = 4;
+FrameCallback frames[] = { drawDateTime, drawCurrentWeather, drawForecast, drawThingspeak, drawNight};
+int numberOfFrames = 5;//4
 
 OverlayCallback overlays[] = { drawHeaderOverlay };
 int numberOfOverlays = 1;
 
 void setup() {
     // Turn On VCC
-  pinMode(D4, OUTPUT);
-  digitalWrite(D4, HIGH);
+  //pinMode(D4, OUTPUT); //No need as we directly connect 5V/GND to OLED
+  //digitalWrite(D4, HIGH);
+  #if defined(DEBUG)
   Serial.begin(115200);
-
+  #endif
   // initialize dispaly
   display.init();
   display.clear();
@@ -180,6 +193,11 @@ void setup() {
   // SLIDE_LEFT, SLIDE_RIGHT, SLIDE_TOP, SLIDE_DOWN
   ui.setFrameAnimation(SLIDE_LEFT);
 
+  //Set the approx. time a frame is displayed
+  ui.setTimePerFrame(4150); //Little longer display
+  //Set the approx. time a transition will take
+  ui.setTimePerTransition(300); //Speed up transistion, before it was 350
+
   ui.setFrames(frames, numberOfFrames);
 
   ui.setOverlays(overlays, numberOfOverlays);
@@ -188,7 +206,9 @@ void setup() {
   ui.init();
 
   // Setup OTA
+  #if defined(DEBUG)
   Serial.println("Hostname: " + hostname);
+  #endif
   ArduinoOTA.setHostname((const char *)hostname.c_str());
   ArduinoOTA.onProgress(drawOtaProgress);
   ArduinoOTA.begin();
@@ -211,7 +231,9 @@ void loop() {
     // You can do some work here
     // Don't do stuff if you are below your
     // time budget.
+
     ArduinoOTA.handle();
+
     delay(remainingTimeBudget);
   }
 
@@ -219,17 +241,19 @@ void loop() {
 }
 
 void configModeCallback (WiFiManager *myWiFiManager) {
+  #if defined(DEBUG)
   Serial.println("Entered config mode");
   Serial.println(WiFi.softAPIP());
   //if you used auto generated SSID, print it
   Serial.println(myWiFiManager->getConfigPortalSSID());
+  #endif
   display.clear();
   display.setTextAlignment(TEXT_ALIGN_CENTER);
   display.setFont(ArialMT_Plain_10);
   display.drawString(64, 10, "Wifi Manager");
   display.drawString(64, 20, "Please connect to AP");
-  display.drawString(64, 30, myWiFiManager->getConfigPortalSSID());
   display.drawString(64, 40, "To setup Wifi Configuration");
+  display.drawString(64, 30, myWiFiManager->getConfigPortalSSID());
   display.display();
 }
 
@@ -262,11 +286,9 @@ void updateData(OLEDDisplay *display) {
   thingspeak.getLastChannelItem(THINGSPEAK_CHANNEL_ID, THINGSPEAK_API_READ_KEY);
   lastUpdate = timeClient.getFormattedTime();
   readyForWeatherUpdate = false;
-  drawProgress(display, 100, "Done...");
+  drawProgress(display, 100, "Done !");
   delay(1000);
 }
-
-
 
 void drawDateTime(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
   display->setTextAlignment(TEXT_ALIGN_CENTER);
@@ -284,19 +306,28 @@ void drawDateTime(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, in
 void drawCurrentWeather(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
   display->setFont(ArialMT_Plain_10);
   display->setTextAlignment(TEXT_ALIGN_LEFT);
-  display->drawString(60 + x, 5 + y, wunderground.getWeatherText());
+  display->drawString(60 + x, 0 + y, wunderground.getWeatherText());//y + 5
 
   display->setFont(ArialMT_Plain_24);
   String temp = wunderground.getCurrentTemp() + "°C";
-  display->drawString(60 + x, 15 + y, temp);
-  int tempWidth = display->getStringWidth(temp);
+  display->drawString(60 + x, 9 + y, temp);
+
+  display->setFont(ArialMT_Plain_10);
+  String precipt = wunderground.getPrecipitationToday();
+  if (precipt.toInt()>0) { //Conditionnal display based on raining
+    display->drawString(60 + x, 30 + y, "Rain: " + precipt +"mm");
+    }
+  else {display->drawString(60 + x, 29 + y, "Hygro: "+wunderground.getHumidity());}
+
+  display->drawString(0 + x, 37 + y, wunderground.getPressure());
+  display->drawString( 50 + x, 37 + y, wunderground.getWindSpeed() +"km/h");
+  display->drawString( 90 + x, 37 + y, wunderground.getWindDir());
 
   display->setFont(Meteocons_Plain_42);
   String weatherIcon = wunderground.getTodayIcon();
   int weatherIconWidth = display->getStringWidth(weatherIcon);
-  display->drawString(32 + x - weatherIconWidth / 2, 05 + y, weatherIcon);
+  display->drawString(32 + x - weatherIconWidth / 2, 01 + y, weatherIcon);
 }
-
 
 void drawForecast(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
   drawForecastDetails(display, x, y, 0);
@@ -328,6 +359,31 @@ void drawForecastDetails(OLEDDisplay *display, int x, int y, int dayIndex) {
   display->setTextAlignment(TEXT_ALIGN_LEFT);
 }
 
+void drawNight(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y){
+  display->setFont(ArialMT_Plain_10);
+  display->setTextAlignment(TEXT_ALIGN_LEFT);
+  display->drawString(36 + x, 0 + y, "Rise:");
+  display->drawString(72 + x, 0 + y, "Fall:");
+  display->drawString(0 + x, 10 + y, "Sun:");
+  display->drawString(0 + x, 18 + y, "Moon:");
+  display->drawString( 0 + x, 37 + y, "Wind :");
+  display->drawString(36 + x, 10 + y, wunderground.getSunsetTime());
+  display->drawString(72 + x, 10 + y, wunderground.getSunriseTime());
+  display->drawString(36 + x, 18 + y, wunderground.getMoonsetTime());
+  display->drawString(72 + x, 18 + y, wunderground.getMoonriseTime());
+  display->drawString(0 + x, 27 + y, wunderground.getMoonPhase() + " ");
+  display->drawString(90 + x, 27 + y,wunderground.getMoonAge() + " j");
+  display->drawString( 34 + x, 37 + y, wunderground.getWindSpeed() +"km/h");
+  display->drawString( 78 + x, 37 + y, wunderground.getWindDir());
+
+  /* //TODO: moon phase picture from bHogan2 idea
+  display->setFont(Meteocons_Plain_21);//42
+  String moonIcon = wunderground.getMoonPctIlum();
+  int moonIconWidth = display->getStringWidth(moonIcon);
+  display->drawString(32 + x - moonIconWidth / 2, 05 + y, moonIcon);
+  */
+}
+
 void drawHeaderOverlay(OLEDDisplay *display, OLEDDisplayUiState* state) {
   display->setColor(WHITE);
   display->setFont(ArialMT_Plain_10);
@@ -338,9 +394,9 @@ void drawHeaderOverlay(OLEDDisplay *display, OLEDDisplayUiState* state) {
   display->setTextAlignment(TEXT_ALIGN_CENTER);
   display->drawString(38, 54, time);
 
-  display->setTextAlignment(TEXT_ALIGN_CENTER);
-  String temp = wunderground.getCurrentTemp() + "°C";
-  display->drawString(90, 54, temp);
+  display->setTextAlignment(TEXT_ALIGN_RIGHT);
+  String temp = wunderground.getCurrentTemp() + "°";
+  display->drawString(106, 54, temp);
 
   int8_t quality = getWifiQuality();
   for (int8_t i = 0; i < 4; i++) {
@@ -375,6 +431,8 @@ int8_t getWifiQuality() {
 }
 
 void setReadyForWeatherUpdate() {
+  #if defined(DEBUG)
   Serial.println("Setting readyForUpdate to true");
+  #endif
   readyForWeatherUpdate = true;
 }
